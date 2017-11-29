b397ef5ec6653c792a824c8cc269c3e5b0111ee8
antske <antske.fokkens@vu.nl>
2017-11-29 09:50:36 +0100

    full revision architecture and corrected regular expressions

--- strict_pattern_identification.py
+++ strict_pattern_identification.py
@@ -2,104 +2,84 @@ import os
 import sys
 import re
 import shutil
+import argparse
 
 
-strictly_red = {}
-strictly_orange = {}
-flexible_red = {}
-flexible_orange = {}
-
-strict_red_age = {}
-strict_orange_age = {}
-flexible_red_age = {}
-flexible_orange_age = {}
 
 
-def dict_initiation(dictname, filename):
+def dict_update(mydict, filename):
 
     key = ''
-    for line in open(filename, 'r'):
+    for line in open('patterns/' + filename, 'r'):
         if '==' in line:
             key = line.split('==')[1]
-            dictname[key] = []
+            mydict[key] = []
         else:
-            dictname[key].append(line.rstrip())
-
-def initiate_dicts():
-    
-    global strictly_red, strictly_orange, flexible_red, flexible_orange
-
-    #TODO: BECOMES STRICT_RED
-    dict_initiation(strictly_red, 'strict_red.txt')
-    dict_initiation(strictly_orange, 'strict_orange.txt')
-    dict_initiation(flexible_red, 'flexible_red.txt')
-    dict_initiation(flexible_orange, 'flexible_orange.txt')
+            mydict[key].append(line.rstrip())
 
 
-def check_final_destinations():
+def create_first_line(classification_dict):
 
-    my_dests = ['strict_reds','strict_oranges','flexible_reds','flexible_oranges']
+    firstLine = 'FileId'
 
-    for dir in my_dests:
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-
-
-def create_first_line():
-
-    global strictly_red, strictly_orange
-
-    firstLine = 'FileId,Aanpak werkloosheid,Stage'
-
-    for k in sorted(strictly_red.keys()):
-        firstLine += ',rood-' + k
-    
-    for k in sorted(strictly_orange.keys()):
-        firstLine += ',oranje-' + k
-
-
-    for k in sorted(flexible_red.keys()):
-        firstLine += ',flexrood-' + k
-    
-    for k in sorted(flexible_orange.keys()):
-        firstLine += ',flexoranje-' + k
+    for k in sorted(classification_dict.keys()):
+        mydict = classification_dict.get(k)
+        for header in sorted(mydict.keys()):
+            firstLine += ',' + header + ' (' + k + ')'
 
     return firstLine + '\n'
 
 
+def interpret_config_file(config):
 
-def initiate_count_dicts():
+    configurations = {}
+    files = []
+    key_name = None
+    for line in open(config, 'r'):
+        if ';' in line:
+            if len(files) > 0 and not key_name is None:
+                configurations[key_name] = files
+                files = []
+            key_name = line.split(';')[1]
+        else:
+            files.append(line.rstrip())
 
-    global strictly_red, strictly_orange, flexible_red, flexible_orange
-    
-    
+    if len(files) > 0 and not key_name is None:
+        configurations[key_name] = files
+    return configurations
+
+
+def initiate_dicts(config):
+    '''
+    Returns a list of dictionaries with classname and regular expressions attached to it
+    :param config: configuration files
+    :return: dictionary of classification dictionaries
+    '''
+
+    configurations = interpret_config_file(config)
+    classification_dicts = {}
+    for classname, files in configurations.items():
+        mydict = {}
+        for filename in files:
+            dict_update(mydict, filename)
+        classification_dicts[classname] = mydict
+
+    return classification_dicts
+
+
+def initiate_count_dicts(classification_dicts):
+
     count_dict = {}
-    
-    count_dict['unemployment'] = 0
-    count_dict['internship'] = 0
-    
-    count_strict_red = {}
-    for k in strictly_red.keys():
-        count_strict_red[k] = 0
-    count_dict['strictRed'] = count_strict_red
 
-    count_strict_orange = {}
-    for k in strictly_orange.keys():
-        count_strict_orange[k] = 0
-    count_dict['strictOrange'] = count_strict_orange
-
-    count_flexible_red = {}
-    for k in flexible_red.keys():
-        count_flexible_red[k] = 0
-        count_dict['flexibleRed'] = count_flexible_red
-    
-    count_flexible_orange = {}
-    for k in flexible_orange.keys():
-        count_flexible_orange[k] = 0
-    count_dict['flexibleOrange'] = count_flexible_orange
-
+    for dictname, value in classification_dicts.items():
+        subdict = {}
+        for k in value.keys():
+            subdict[k] = 0
+        count_dict[dictname] = subdict
 
     return count_dict
+
+
 
 
 def update_age_dict(line, myregex, mydict, k):
@@ -122,12 +102,29 @@ def update_age_dict(line, myregex, mydict, k):
                 myval[foundnumber] = 1
 
 
+def analyze_file(filename, count_dict, pattern_dicts):
 
-def analyze_file(filename, count_dict):
+    values = {}
+    text = ''
+    for line in open(filename, 'r'):
+        newline = line
+        for classname, pdict in pattern_dicts.items():
+            for k in pdict.keys():
+                if k in line.lower():
+                    for v in pdict.get(k):
+                        if re.match(v, line.lower()):
+                            rx = re.compile(v)
+                            newline = re.sub(rx, r'\1<span><b>\2</b></span>\3', line.lower())
+                            mycount_dict = count_dict.get(classname)
+                            mycount_dict[k] += 1
+                            values[classname] = None
+        text += newline
+    for key in values:
+        values[key] = text
+    return values
 
-    global strictly_red, strictly_orange, flexible_red, flexible_orange
-    
-    global strictly_red_age, strictly_orange_age, flexible_red_age, flexible_orange_age
+def analyze_file_old(filename, count_dict):
+
     
     values = [False, False, False, False]
 
@@ -171,70 +168,85 @@ def analyze_file(filename, count_dict):
 
 def create_output_values(count_dict):
 
-    outvalues = ',' + str(count_dict.get('unemployment')) + ',' + str(count_dict.get('internship'))
-    red_strict = count_dict.get('strictRed')
-    for k in sorted(red_strict.keys()):
-        outvalues += ',' + str(red_strict.get(k))
-    
-    orange_strict = count_dict.get('strictOrange')
-    for k in sorted(orange_strict.keys()):
-        outvalues += ',' + str(orange_strict.get(k))
-
-
-    red_flex = count_dict.get('flexibleRed')
-    for k in sorted(red_flex.keys()):
-        outvalues += ',' + str(red_flex.get(k))
-
-    orange_flex = count_dict.get('flexibleOrange')
-    for k in sorted(orange_flex.keys()):
-        outvalues += ',' + str(orange_flex.get(k))
+    outvalues = ''
+    for dkey in sorted(count_dict.keys()):
+        mydict = count_dict.get(dkey)
+        for k in sorted(mydict.keys()):
+            outvalues += ',' + str(mydict.get(k))
 
     return outvalues
 
+def create_outdirectories(classification_dicts, outdir):
 
 
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
 
-def classify_files(inputdir, outputfile = None):
+    for k in classification_dicts.keys():
+        if not os.path.exists(outdir + '/' + k):
+            os.makedirs(outdir + '/' + k)
 
-    if outputfile is None:
-        outputfile = 'classifications.csv'
-    
-    myout = open(outputfile, 'w')
-    myout.write(create_first_line())
+
+def write_outfiles(infilename, outdir, values):
+
+    for val, text in values.items():
+        myoutput = open(outdir + '/' + val + '/' + infilename, 'w')
+        myoutput.write(text)
+        myoutput.close()
+
+
+def classify_files(args):
+
+
+    classification_dicts = initiate_dicts(args.config)
+    myout = open(args.outputfile, 'w')
+    #create outputfiles if outdir is given
+    create_verification_data = False
+    if not args.outdir is None:
+        create_verification_data = True
+        create_outdirectories(classification_dicts, args.outdir)
+    #FIXME use CSV library
+    myout.write(create_first_line(classification_dicts))
+
+    inputdir = args.inputdir
 
     for f in os.listdir(inputdir):
-        count_dict = initiate_count_dicts()
-        values = analyze_file(inputdir + f, count_dict)
+        count_dict = initiate_count_dicts(classification_dicts)
+        values = analyze_file(inputdir + f, count_dict, classification_dicts)
         outvalues = create_output_values(count_dict)
         myout.write(f + outvalues + '\n')
-        if values[0]:
-            shutil.copy(inputdir + f, 'strict_reds/')
-        if values[1]:
-            shutil.copy(inputdir + f, 'strict_oranges/')
-        if values[2]:
-            shutil.copy(inputdir + f, 'flexible_reds/')
-        if values[3]:
-            shutil.copy(inputdir + f, 'flexible_oranges/')
+        #FIXME create marked-up output (values dictionary, key with identified text
+        if create_verification_data:
+            if len(values) > 0:
+                write_outfiles(f, args.outdir, values)
+
+       # if values[0]:
+       #     shutil.copy(inputdir + f, 'strict_reds/')
+       # if values[1]:
+       #     shutil.copy(inputdir + f, 'strict_oranges/')
+       # if values[2]:
+       #     shutil.copy(inputdir + f, 'flexible_reds/')
+       # if values[3]:
+       #     shutil.copy(inputdir + f, 'flexible_oranges/')
 
 
 
 
 
-def main(argv=None):
+def main():
 
-    if argv is None:
-        argv = sys.argv
+    parser = argparse.ArgumentParser(description='Aims to identify various forms of age discrimination in job advertisements')
+    parser.add_argument('inputdir', metavar='inputdir', help='path to directory containing the job advertisements')
+    parser.add_argument('outputfile', metavar='outputfile', help='path to outputfile providing statistics from analysis')
+    parser.add_argument('--outdir', metavar='outputdir', default=None, help='path to output directory (created if does not exist), if placing advertisememts in subdirectories based on their classification is desired (default=None)')
+    parser.add_argument('--config', help='path to configuration file (default: local \'config\')', default='config')
 
-    if len(argv) < 2:
-        print('Usage: python red_orange_identification.py inputdir/')
-    elif len(argv) < 3:
-        initiate_dicts()
-        check_final_destinations()
-        classify_files(argv[1])
-    else:
-        initiate_dicts()
-        check_final_destinations()
-        classify_files(argv[1], argv[2])
+    args = parser.parse_args()
+
+    classify_files(args)
+
+
+
 
 
 if __name__ == '__main__':
