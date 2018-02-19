716fe763182bfc37301e0fdd0145a6b0a8a3d57b
antske <antske.fokkens@vu.nl>
2018-02-19 16:24:16 +0100

    fixed irregularities in patterns and solved mismatch patterns classification code

--- strict_pattern_identification.py
+++ strict_pattern_identification.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 
 
 
-age_discriminating_categories = ['min','student','leeftijd_vanaf','leeftijd_bepaald','ouder']
+age_discriminating_categories = ['min','stud','leeftijd_vanaf','leeftijd_bepaald','ouder']
 
 
 
